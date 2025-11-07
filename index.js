18a7c934fe16c2dae5eb238d721995bcfc520e3f
officerkiruha <kirillkovalchukstudy@gmail.com>
2025-11-07 09:13:13 +0200

    add folder public with js,css,html

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-const app = express();
 const port = process.env.PORT;
+const api = process.env.HOST;
+const app = express();
+app.use(express.static(__dirname));
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
 
-
-app.get()
-
-
-
-app.listen(port,()=>{console.log(`htpp://${process.env.HOST}:${port}`)});
+app.listen(port,()=>{console.log(`http://${api}:${port}`)});
