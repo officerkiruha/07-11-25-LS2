0282a76de5e745f8d0f53bef1bf734b6951bb141
officerkiruha <kirillkovalchukstudy@gmail.com>
2025-11-07 10:09:57 +0200

    add db config and install mysql2 and create pool

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT;
 const api = process.env.HOST;
 const app = express();
 app.use(express.static(__dirname));
+const db = require('./config/db_config');
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html')
